84c43adaaa2f1e2639bdba79009686fc7c037636
HossamSadekk <66652255+HossamSadekk@users.noreply.github.com>
2022-08-17 05:37:18 -0700

    Installing room with dependency injection

---
+++ app/src/main/java/com/example/todoapp/data/TaskDao.kt
@@ -0,0 +1,20 @@
+package com.example.todoapp.data
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TaskDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE) // this to define what will happen i insert a task with the same if of another task
+    suspend fun insert(task: Task)
+
+    @Update
+    suspend fun update(task: Task)
+
+    @Delete
+    suspend fun delete(task: Task)
+
+    @Query("SELECT * FROM task_table")
+    fun getTasks(): Flow<List<Task>>
+}
